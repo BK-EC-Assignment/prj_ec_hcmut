3ce0e2c64c286f77c2b48b72d5bfd7509211c411
luongquocdinh <luongquocdinh.95@gmail.com>
2016-10-21 01:26:41 +0700

    add fields numDay

--- modules/addProduct/commitData.js
+++ modules/addProduct/commitData.js
@@ -2,10 +2,16 @@ var database = require('../../models/index.js').database
 var fs = require('fs')
 
 module.exports = function (input, fields, files) {
-  if ((fields.name) && (files.picture) && (fields.description) && (fields.time)) {
+  if ((fields.name) && (files.picture) && (fields.description) && (fields.date)) {
     var path = files.picture.path
+    var oneDay = 24*60*60*1000
+    var date = new Date(fields.date)
+    var now = new Date()
+    console.log(date)
+    console.log(now)
     var data = {
-      time: fields.time,
+      date: fields.date,
+      numDay: Math.round(Math.abs((date.getTime() - now.getTime())/(oneDay))),
       description: (fields.description) ? fields.description : '',
       status: true,
       name: fields.name,
@@ -18,11 +24,9 @@ module.exports = function (input, fields, files) {
     return database.create('VERTEX', 'Product').set(data).one()
       .then(
         function (value) {
-          console.log(value)
           return input.success(input.done)
         },
         function () {
-          console.log('error')
           return input.errHandle(input.done)
         })
   } else {
